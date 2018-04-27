4e4422f4955ef6736c0dbb69f910a19f548b5bbc
THEBALLLLLY <shadowangle1222@hotmail.com>
2018-04-27 17:03:19 +0700

    update commend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_26_104231) do
+ActiveRecord::Schema.define(version: 2018_04_27_095856) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
